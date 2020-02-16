d316e2de3b32b05fee0cba0efb166f29b045a490
Yuri Chornoivan <yurchor@ukr.net>
2020-02-16 11:46:08 +0200

    Extract messages from header file as well

--- Messages.sh
+++ Messages.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp` -o $podir/plasmavault-kde.pot
+$XGETTEXT `find . -name \*.js -o -name \*.qml -o -name \*.cpp -o -name \*.h` -o $podir/plasmavault-kde.pot
 rm -f rc.cpp
