KDE/plasma-vault

commit 297db6318e3ec4ea85c695fc195121046742826a

Set saner width for Placeholder message so its text doesn't overflow
See D29692

plasma/package/contents/ui/main.qml
@@ -81,9 +81,7 @@ Item {
                 id: noVaultsMessage
 
                 anchors.centerIn: parent
-                anchors.left: parent.left
-                anchors.right: parent.right
-                anchors.margins: units.largeSpacing
+                width: parent.width - (units.largeSpacing *
 
                 visible: vaultsList.count === 0
 

commit 20c059b3dbf673c4fa76dedfedca9a33fb2e03a7

Fix broken code
Introduced in 297db6318e3ec4ea85c695fc195121046742826a, sorry about that

plasma/package/contents/ui/main.qml
@@ -81,7 +81,7 @@ Item {
                 id: noVaultsMessage
 
                 anchors.centerIn: parent
-                width: parent.width - (units.largeSpacing *
+                width: parent.width - (units.largeSpacing * 4)
 
                 visible: vaultsList.count === 0
 