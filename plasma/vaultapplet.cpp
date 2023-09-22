e47b1ec103c8941e7d547124eeb0025e13f6f9dc
Nicolas Fella <nicolas.fella@gmx.de>
2023-09-22 22:04:58 +0200

    Remove plugin metadata from applet C++ plugin
    It's not needed and causes a warning

--- plasma/vaultapplet.cpp
+++ plasma/vaultapplet.cpp
@@ -31,7 +31,7 @@ QObject *VaultApplet::vaultsModel()
     return m_vaultsModel;
 }
 
-K_PLUGIN_CLASS_WITH_JSON(VaultApplet, "package/metadata.json")
+K_PLUGIN_CLASS(VaultApplet)
 
 #include "vaultapplet.moc"
 
